39ee9709101a05ebbf6fe4b460989886066da03b
Stephen Simon <Stephen Simon>
2017-05-19 16:52:47 -0500

    Nicer plots for 6-beta-extension

--- 0-preliminaries.R
+++ 0-preliminaries.R
@@ -12,14 +12,15 @@ options(mc.cores = parallel::detectCores())
 opts_chunk$set(
   echo=FALSE,
   message=FALSE,
-  warning=FALSE,
-  fig.width=5,
-  fig.height=1)
+  warning=FALSE)
+
+pctl_list <- c(1, 25, 50, 75, 99)
+
 
 custom_boxplot <- function(df, y_label, rounding_level, co="black") {
   df                                            %>%
     use_series(y)                               %>%
-    quantile(c(0.01, 0.25, 0.5, 0.75, 0.99))    -> tm
+    quantile(pctl_list/100)                     -> tm
   tm                                            %>%
     round(rounding_level)                       -> lb
   df                                            %>%
@@ -34,6 +35,31 @@ custom_boxplot <- function(df, y_label, rounding_level, co="black") {
     xlab(" ")                                    +
     ylab(y_label)                                +
     coord_flip()                                %>%
+    return
+}
+
+custom_scatterplot <- function(df, x_name, y_name, round_x=1, round_y=0) {
+  df                                              %>%
+    use_series(x)                                 %>%
+    quantile(pctl_list/100)                       -> x_ticks
+  x_ticks                                         %>%
+    round(round_x)                                -> x_labels
+  df                                              %>%
+    use_series(y)                                 %>%
+    quantile(pctl_list/100)                       -> y_ticks
+  y_ticks                                         %>%
+    round(0)                                      -> y_labels
+  df                                              %>%
+    ggplot(aes(x, y))                              +
+    xlab(x_name)                                   +
+    ylab(y_name)                                   +
+    scale_x_continuous(breaks=x_ticks,
+                       minor_breaks=NULL,
+                       labels=x_labels)            +
+    scale_y_continuous(breaks=y_ticks,
+                       minor_breaks=NULL,
+                       labels=y_labels)            +
+    geom_point()                                  %>%
     return
 }
 
